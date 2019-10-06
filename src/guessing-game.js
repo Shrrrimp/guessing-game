807b1de605fa9115f4b822113c0999e36f531326
julia tokmakova <julia_tokmakova00@mail.ru>
2019-10-06 23:04:34 +0300

    add Guessing game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
+    //делим промежуток на половинку    
     guess() {
-
+        this.center = Math.round((this.max + this.min)/2);
+        return this.center;
     }
-
+    //если больше заданного числа, то уменьшаем максимальное значение => уменьшаем промежуток   
     lower() {
-
+        this.max = this.center;
     }
-
+        
     greater() {
-
+        this.min = this.center;
     }
 }
 
